a5967f29dbfbe327d4ac071533850c162b2024a3
hello_grimm <91032711+helloGrimm@users.noreply.github.com>
2023-05-18 02:11:10 +0200

    Add files via upload

--- DumDom/scripts/background.js
+++ DumDom/scripts/background.js
@@ -1,11 +1,11 @@
 // Skrypt odpowiedzialny za zmianę tła na stronie głównej 
 var backgroundsImg = new Array (
-    "../images/background/Neon_Genesis_Evangelion_4.png",
-    "../images/background/Kaguya_sama_Love_Is_War_1.png",
-    "../images/background/Chainsaw_man_1.png",
-    "../images/background/One_Piece_1.png",
-    "../images/background/Kakegurui_1.png",
-    "../images/background/hunter_x_hunter_1.png"
+    "../DumDom/images/background/Neon_Genesis_Evangelion_4.png",
+    "../DumDom/images/background/Kaguya_sama_Love_Is_War_1.png",
+    "../DumDom/images/background/Chainsaw_man_1.png",
+    "../DumDom/images/background/One_Piece_1.png",
+    "../DumDom/images/background/Kakegurui_1.png",
+    "../DumDom/images/background/hunter_x_hunter_1.png"
 );
 
 // Odstęp po między następnym tłem (wartość w milisekundeach) 
