08cf5fb58cc60cdd4264ee6efc141a38190b26db
hello_grimm <91032711+helloGrimm@users.noreply.github.com>
2023-05-29 00:55:52 +0200

    Add files via upload

--- DumDom/pages/Neon_Genesis_Evangelion/scripts/sort.js
+++ DumDom/pages/Neon_Genesis_Evangelion/scripts/sort.js
@@ -1,5 +1,8 @@
-function sortDivs () {
+function sort () {
+    var toSort = document.getElementById('characters').children;
+    // https://stackoverflow.com/questions/7056925/how-does-array-prototype-slice-call-work
+    toSort = Array.prototype.slice.call(toSort, 0);
 
 }
 
-sortDivs();
+sort()
